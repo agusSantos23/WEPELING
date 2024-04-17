275f9f31be4ac3f4beacf684cc3eef21329eeaa4
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-04-17 08:23:46 +0200

    1.2

--- php/crearUF.php
+++ php/crearUF.php
@@ -1,11 +1,23 @@
 <?php
 include("./conn.php");
 
-    $conn = conectar();
+$conn = conectar();
 
-    
+$nombre = $_POST['nombre'];
 
-    
-?>
+$contra = $_POST['contra'];
+
+$contra_Hashed = password_hash($contra, PASSWORD_DEFAULT);
+
+
+$sql = "INSERT INTO usuario (nombre,PASSWORD) VALUES ('$nombre','$contra_Hashed')";
+
+if($conn->query($sql)){
+    echo "Registrado";
+}else{
+    echo "No Registrado " . $conn->error;
+}
+
+$conn->close();
 
 
