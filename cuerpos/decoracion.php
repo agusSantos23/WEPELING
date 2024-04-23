ee9f19102ce7f5495e85904906e215d90da1546a
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-04-23 10:33:46 +0200

    1.6
    Muchos fallos

---
+++ cuerpos/decoracion.php
@@ -0,0 +1,15 @@
+    
+    
+    <div class="lineasI">
+        <div class="fino r"></div>
+        <div class="grueso r"></div>
+    </div>
+
+    <div class="lineasD">
+        <div class="fino r"></div>
+        <div class="grueso r"></div>
+    </div>
+
+    <img src="" alt="Luna" class="luna">
+
+    <img src="" alt="Dirigible" class="dirigible">
